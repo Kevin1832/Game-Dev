caa09f3c654545785e924a08067dd811ed7f7cea
Kevin1832 <justcallmekev2000@gmail.com>
2024-03-25 01:25:31 -0500

    3

--- FinalProject/Assets/CharacterMovement.cs
+++ FinalProject/Assets/CharacterMovement.cs
@@ -1,31 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] string CreatureName = "GraveRobber";
     [SerializeField] float speed = 0f;
+    float inputHorizontal;
+    float inputVertical;
+    bool faceRight = true;
     [SerializeField] GameObject square;
     [SerializeField] float jumpForce = 10;
 
     public enum CharacterMovementType { tf, physics };
     [SerializeField] CharacterMovementType movementType = CharacterMovementType.tf;
     [SerializeField] Vector3 homePosition = Vector3.zero;
-    
+
     CharacterInput characterInput;
 
     Rigidbody2D rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
+    void FlipCharacter()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        if (inputHorizontal != 0)
+        {
+            rb.AddForce(new Vector2(inputHorizontal * speed, 0f));
+        }
+        if (inputHorizontal > 0 && !faceRight)
+        {
+            FlipUpdated();
+        }
+        if (inputHorizontal < 0 && faceRight)
+        {
+            FlipUpdated();
+        }
+    }
+
+    void FlipUpdated()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        faceRight = !faceRight;
     }
 
     public void MoveCreature(Vector3 direction)
     {
-
         if (movementType == CharacterMovementType.tf)
         {
             MoveCharacterTransform(direction);
@@ -34,14 +64,12 @@ public class CharacterMovement : MonoBehaviour
         {
             MoveCharacterRigidbody2D(direction);
         }
-
     }
+
     public void MoveCharacterRigidbody2D(Vector3 direction)
     {
         Vector3 currentVelocity = new Vector3(0, rb.velocity.y, 0);
         rb.velocity = (currentVelocity) + (direction * speed);
-
-        
     }
 
     public void MoveCharacterTransform(Vector3 direction)
@@ -51,7 +79,6 @@ public class CharacterMovement : MonoBehaviour
 
     public void Jump()
     {
-         rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
     }
-
 }
